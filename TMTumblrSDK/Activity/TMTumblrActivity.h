889a9606509afbc48d2322e3c3abc4c9423eb921
Benjamin Jackson <bhjackson@gmail.com>
2013-05-06 16:16:00 -0400

    Updated TMTumblrActivity to use the new TMTumblrAppClient class to post a link.

--- TMTumblrSDK/Activity/TMTumblrActivity.h
+++ TMTumblrSDK/Activity/TMTumblrActivity.h
@@ -8,14 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-/**
- `TMTumblrActivity` provides the bare minimum `UIActivity` subclass necessary to start sharing to Tumblr: just the name
- and icon.
- 
- For now this is essentially a blank canvas to be used in conjunction with either `TMAPIClient` or `TMTumblrAppClient`.
- This should be subclassed in accordance with the `UIActivity` documentation to actually perform an action based on
- provided activity items. In the future we may provide a more integrated solution.
- */
-@interface TMTumblrActivity : UIActivity
-
+@interface TMTumblrActivity : UIActivity <UIAlertViewDelegate>
+@property (nonatomic) NSString *title;
 @end
--- TMTumblrSDK/Activity/TMTumblrActivity.m
+++ TMTumblrSDK/Activity/TMTumblrActivity.m
@@ -7,12 +7,14 @@
 //
 
 #import "TMTumblrActivity.h"
+#import "TMTumblrAppClient.h"
+
+@interface TMTumblrActivity ()
+@property (nonatomic) NSURL *linkURL;
+@property (nonatomic) NSString *linkDescription;
+@end
 
 @implementation TMTumblrActivity
-
-- (BOOL)canPerformWithActivityItems:(NSArray *)activityItems {
-    return YES;
-}
 
 - (NSString *)activityType {
 	return NSStringFromClass([self class]);
@@ -24,6 +26,52 @@
 
 - (UIImage *)activityImage {
 	return [UIImage imageNamed:@"UIActivityTumblr"];
+}
+
+- (BOOL)canPerformWithActivityItems:(NSArray *)activityItems {
+    for (UIActivityItemProvider *item in activityItems) {
+        if ([item isKindOfClass:[NSURL class]]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (void)prepareWithActivityItems:(NSArray *)activityItems {
+    for (id item in activityItems) {
+        if ([item isKindOfClass:[NSURL class]] && !self.linkURL) {
+            self.linkURL = item;
+        }
+        if ([item isKindOfClass:[NSString class]] && !self.linkDescription) {
+            self.linkDescription = item;
+        }
+    }
+}
+
+
+
+- (void)performActivity {
+    if ([TMTumblrAppClient isTumblrInstalled]) {
+        NSString *description = [self.linkDescription length] ? self.linkDescription : @"";
+        [TMTumblrAppClient createLinkPost:self.title
+                                URLString:[self.linkURL absoluteString]
+                              description:description
+                                     tags:@[]];
+    }
+    else {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Tumblr App Required"
+                                                        message:@"Please install the Tumblr app and try again."
+                                                       delegate:self
+                                              cancelButtonTitle:@"Cancel"
+                                              otherButtonTitles:@"Install", nil];
+        [alert show];
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 1) {
+        [TMTumblrAppClient viewInAppStore];
+    }
 }
 
 @end
